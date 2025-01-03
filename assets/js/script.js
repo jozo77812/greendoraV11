ff79690f229c2b6a05c8780c8635cfe54c517e6f
jozo77812 <jozo77812@gmail.com>
2025-01-03 10:40:00 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,5 @@ function addToCart(productName, productPrice) {
 
 function updateCartDisplay() {
     console.clear();
-    console.log("Aktuálny košík:", cart);
-
-    let total = 0;
-    cart.forEach(item => {
-        total += item.price * item.quantity;
-        console.log(`${item.name} - ${item.quantity} x ${item.price.toFixed(2)}€`);
-    });
-
-    console.log(`Celková suma: ${total.toFixed(2)}€`);
+    console.log("Košík:", cart);
 }
